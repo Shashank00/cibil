14ca740dcb82481aa59e4b547746fde7e3634086
hasher <you@example.com>
2020-11-20 12:10:09 +0530

    Updated

--- src/main/java/com/axis/cibil/controllers/CibilController.java
+++ src/main/java/com/axis/cibil/controllers/CibilController.java
@@ -5,33 +5,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.TreeMap;
 
 @RestController
 public class CibilController {
 
     @RequestMapping(value= "/cibilPost", method = RequestMethod.POST)
-    public Map<String, String> getIdmResponse(@RequestBody String request) throws Exception {
-        Map<String, String> response = new HashMap<>();
+    public Map<String, String> getCibilResponse(@RequestBody String request) throws Exception {
+        Map<String, String> response = new TreeMap<>();
 
         if(checkFailure() || request == null) {
             throw new Exception();
         }
+
+
         else {
             response.put("status", "Approved");
             response.put("cibilValueFirst", "999");
             response.put("cibilValueSecond", "007");
         }
+        try{
+            Thread.sleep(5000);
+        } catch(InterruptedException e) {
 
+        }
         return response;
     }
 
     private Boolean checkFailure() {
         Random random = new Random();
         int value = random.nextInt(100);
-        if(value%9 == 0) {
+        if(value%11 == 0) {
             return true;
         }
         return false;
